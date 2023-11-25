6c0c2bf74380be4157db80d2042d2a289c2a8bf6
Vic Wong <vicwong@gmail.com>
2023-11-25 11:38:41 -0800

    Fix various issues transcoding pi video, scope cdg temp dir to the process, fix overlay flag always true

--- start-pi.sh
+++ start-pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/pi/pikaraoke/.venv/bin/activate
-python3 app.py -y /home/pi/pikaraoke/.venv/bin/yt-dlp -d ~/songs 
+python3 app.py -y /home/pi/pikaraoke/.venv/bin/yt-dlp --hide-splash -l10
