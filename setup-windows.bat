e0e405ce87e1c144d04c6cdf7abea1a9527cbfef
Vic Wong <vicwong@gmail.com>
2023-12-02 09:44:26 -0800

    Attempt at windows installs bat scripts

---
+++ setup-windows.bat
@@ -0,0 +1,29 @@
+#!/bin/bash
+@echo off
+
+:start
+echo Are you sure you want to setup PiKaraoke? (y/n):
+set /p confirm=
+
+if /i "%confirm%" == "y" goto setup
+if /i "%confirm%" == "n" goto end
+
+:setup
+
+echo
+echo "*** CREATING PYTHON VIRTUAL ENVIRONMENT ***"
+python3 -m venv .venv
+.venv\Scripts\activate
+
+echo
+echo "*** INSTALLING PYTHON DEPENDENCIES ***"
+pip3 install -r requirements.txt
+
+echo
+echo "*** DONE ***"
+echo "Run PiKaraoke with: ./pikaraoke.bat <args>"
+echo
+
+:end
+
+
