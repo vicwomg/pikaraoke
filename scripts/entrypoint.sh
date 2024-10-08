3e3dbc06d979596a03c175bee85bd8b86399a266
Tom Wozniakowski <tom.wozniakowski@transferwise.com>
2024-10-08 20:43:42 +0100

    293 - Expose 5556, fix entrypoint.sh for video streaming

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,26 +2,9 @@
 
 figlet PiKaraoke
 
-# Read Docker host's hostname if URL is not set
-if [ -z "$URL" ]; then
-    if [ -f /etc/docker_hostname ]; then
-        URL=$(cat /etc/docker_hostname):5555
-        echo "URL is set to Docker Hostname: $URL, open your players browser to http://$URL:5555/splash"
-    else
-        echo "URL was not set and Docker hostname could not be found. Users and guests will need to manually connect to http://(docker hostname):5555. The displayed QR code will not function properly."
-        URL_VARIABLE=""
-    fi
-else
-    URL_VARIABLE="-u $URL"
-fi
-
-cd pikaraoke
 # Run pikaraoke with necessary parameters
-if [ -z "$PASSWORD" ]; then
-    poetry run pikaraoke -d /pikaraoke-songs/ --headless $URL_VARIABLE
-else
-    poetry run pikaraoke -d /pikaraoke-songs/ --headless $URL_VARIABLE --admin-password $PASSWORD
-fi
+cd pikaraoke
+poetry run pikaraoke -d /pikaraoke-songs/ --headless --url $URL:5555 --ffmpeg-url $URL:5556
 
 # Keep the container running
 tail -f /dev/null
