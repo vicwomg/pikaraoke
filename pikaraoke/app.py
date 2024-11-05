79f275977a91959e56e040e62c3f5204a903ac4d
Vic Wong <vicwong@gmail.com>
2024-11-05 00:38:38 -0800

    Adjust message again

--- pikaraoke/app.py
+++ pikaraoke/app.py
@@ -949,9 +949,9 @@ def main():
             elem.click()
         except Exception as e:
             print(
-                f"[ERROR] Error starting splash screen. If you're running headed mode over SSH, you may need to run `export DISPLAY=:0.0` first to target the proper screen. Example: `export DISPLAY=:0.0; pikaraoke`"
+                f"\n[ERROR] Error starting splash screen. If you're running headed mode over SSH, you may need to run `export DISPLAY=:0.0` first to target the proper screen. Example: `export DISPLAY=:0.0; pikaraoke`\n"
             )
-            # cherrypy.engine.exit()
+            cherrypy.engine.exit()
             sys.exit()
 
     # Start the karaoke process
