a9daeb3f8809537d90450f0f60365497d758586d
Leonardo Moreira <lvmasterrj@gmail.com>
2024-12-03 15:09:56 -0300

    fix: File was being deleted even if it was in queue

--- pikaraoke/app.py
+++ pikaraoke/app.py
@@ -441,7 +441,8 @@ def start_song():
 def delete_file():
     if "song" in request.args:
         song_path = request.args["song"]
-        if song_path in k.queue:
+        exists = any(item.get('file') == song_path for item in k.queue)
+        if exists:
             flash(
                 "Error: Can't delete this song because it is in the current queue: " + song_path,
                 "is-danger",
