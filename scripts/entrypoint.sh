43dbccc4fa9bab4728fd849b7c8bf5738396deb2
Tom Wozniakowski <tom.wozniakowski@transferwise.com>
2024-12-21 16:41:51 +0000

    293 - Fix network config (use host!)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ figlet PiKaraoke
 
 # Run pikaraoke with necessary parameters
 cd pikaraoke
-poetry run pikaraoke -d /pikaraoke-songs/ --headless --url $URL:5555 --ffmpeg-url $URL:5556
+poetry run pikaraoke -d /pikaraoke-songs/ --headless --high-quality
 
 # Keep the container running
 tail -f /dev/null
