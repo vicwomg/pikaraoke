0f63418b1ffba0cc6d2103442ead7b474ae42c41
fan9704 <cxz123499@gmail.com>
2025-07-19 15:27:51 +0800

    feat: Add new settings user preferred language

--- pikaraoke/routes/info.py
+++ pikaraoke/routes/info.py
@@ -3,6 +3,7 @@ import psutil
 from flask import Blueprint, render_template
 
 from pikaraoke import VERSION
+from pikaraoke.constants import LANGUAGES
 from pikaraoke.lib.current_app import (
     get_admin_password,
     get_karaoke_instance,
@@ -24,6 +25,9 @@ def info():
     url = k.url
     admin_password = get_admin_password()
     is_linux = get_platform() == "linux"
+    
+    # 獲取當前偏好語言
+    preferred_language = k.get_user_preference("preferred_language", "en")
 
     # cpu
     try:
@@ -54,33 +58,34 @@ def info():
         site_title=site_name,
         title="Info",
         url=url,
-        memory=memory,
-        cpu=cpu,
-        disk=disk,
+        admin=is_admin(),
+        admin_password=admin_password,
+        platform=k.platform,
+        os_version=k.os_version,
         ffmpeg_version=k.ffmpeg_version,
         is_transpose_enabled=k.is_transpose_enabled,
         youtubedl_version=youtubedl_version,
-        platform=k.platform,
-        os_version=k.os_version,
-        is_pi=k.is_raspberry_pi,
-        is_linux=is_linux,
         pikaraoke_version=VERSION,
-        admin=is_admin(),
-        admin_enabled=admin_password != None,
+        cpu=cpu,
+        memory=memory,
+        disk=disk,
+        is_linux=is_linux,
+        volume=int(k.volume * 100),
+        bg_music_volume=int(k.bg_music_volume * 100),
         disable_bg_music=k.disable_bg_music,
-        bg_music_volume=int(100 * k.bg_music_volume),
         disable_bg_video=k.disable_bg_video,
         disable_score=k.disable_score,
-        hide_url=k.hide_url,
-        limit_user_songs_by=k.limit_user_songs_by,
-        avsync=k.avsync,
         hide_notifications=k.hide_notifications,
+        hide_url=k.hide_url,
         hide_overlay=k.hide_overlay,
-        normalize_audio=k.normalize_audio,
-        complete_transcode_before_play=k.complete_transcode_before_play,
-        high_quality=k.high_quality,
-        splash_delay=k.splash_delay,
         screensaver_timeout=k.screensaver_timeout,
-        volume=int(100 * k.volume),
+        splash_delay=k.splash_delay,
+        normalize_audio=k.normalize_audio,
+        high_quality=k.high_quality,
+        complete_transcode_before_play=k.complete_transcode_before_play,
+        avsync=k.avsync,
+        limit_user_songs_by=k.limit_user_songs_by,
         buffer_size=k.buffer_size,
+        languages=LANGUAGES,
+        preferred_language=preferred_language,  # 傳遞當前偏好語言
     )
