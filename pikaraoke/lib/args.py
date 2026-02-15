0846e94e97b21068aa93e29776888a1d6ae80861
Vicwomg <vicwong@gmail.com>
2026-02-15 11:16:46 -0800

    feat: indonesian translation (via @funkyfone) (#773)

--- pikaraoke/lib/args.py
+++ pikaraoke/lib/args.py
@@ -173,7 +173,7 @@ def parse_pikaraoke_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--preferred-language",
-        help="Set the preferred language for the web interface. This will persist across restarts. Available codes: en, de_DE, es_VE, fi_FI, fr_FR, it_IT, ja_JP, ko_KR, nl_NL, no_NO, pt_BR, ru_RU, th_TH, zh_Hans_CN, zh_Hant_TW",
+        help="Set the preferred language for the web interface. This will persist across restarts. Available codes: en, de_DE, es_VE, fi_FI, fr_FR, id_ID,it_IT, ja_JP, ko_KR, nl_NL, no_NO, pt_BR, ru_RU, th_TH, zh_Hans_CN, zh_Hant_TW",
         default=None,
         required=False,
     )
